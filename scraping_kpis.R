ea4dea76a5c6664eb3633b571a30896554f3a540
Maggie Lou <margaretlou2019@u.northwestern.edu>
2016-08-08 15:24:49 +0200

    Add scraping code.

--- scraping_kpis.R
+++ scraping_kpis.R
@@ -72,10 +72,10 @@ collect_kpis <- function() {
   weekly_kpis$new_projects_per_user <- new_projects_per_user()
   
   #all retention data 
-  weekly_kpis$n_retention_1 <- n_retention(1)
-  weekly_kpis$n_retention_3 <- n_retention(3)
-  weekly_kpis$n_retention_7 <- n_retention(7)
-  weekly_kpis$n_retention_30 <- n_retention(30)
+  weekly_kpis$n_retention_1 <- n_retention_day1(browser)
+  weekly_kpis$n_retention_3 <- n_retention_day3(browser)
+  weekly_kpis$n_retention_7 <- n_retention_day7(browser)
+  weekly_kpis$n_retention_30 <- n_retention_day30(browser)
   weekly_kpis$unbounded_retention_1 <- unbounded_retention(1)
   weekly_kpis$unbounded_retention_3 <- unbounded_retention(3)
   weekly_kpis$unbounded_retention_7 <- unbounded_retention(7)
@@ -200,3 +200,92 @@ active_users <- function(browser) {
   return(total)
 }
 
+
+#weekly number of paying users 
+paying_users <- function(browser) {
+  browser$navigate("https://amplitude.com/app/146509/revenue/summary?e=%7B%22event_type%22:%22revenue_amount%22,%22filters%22:%5B%5D,%22group_by%22:%5B%5D%7D&m=paying&sset=%7B%22segmentIndex%22:0%7D&vis=line&range=Last%207%20Days&i=1")
+  
+  total <- 0
+  
+  day_1 <- browser$findElement(using = 'css selector', '#main > div > div.main-content.ng-scope > div > div.ng-scope > react-component > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div')
+  total <- total + as.integer(gsub(",", "", day_1$getElementText()[[1]]))
+  
+  day_2 <- browser$findElement(using = 'css selector', '#main > div > div.main-content.ng-scope > div > div.ng-scope > react-component > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div')
+  total <- total + as.integer(gsub(",", "", day_2$getElementText()[[1]]))
+  
+  day_3 <- browser$findElement(using = 'css selector', '#main > div > div.main-content.ng-scope > div > div.ng-scope > react-component > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(3) > div:nth-child(2) > div')
+  total <- total + as.integer(gsub(",", "", day_3$getElementText()[[1]]))
+  
+  day_4 <- browser$findElement(using = 'css selector', '#main > div > div.main-content.ng-scope > div > div.ng-scope > react-component > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(4) > div:nth-child(2) > div')
+  total <- total + as.integer(gsub(",", "", day_4$getElementText()[[1]]))
+  
+  day_5 <- browser$findElement(using = 'css selector', '#main > div > div.main-content.ng-scope > div > div.ng-scope > react-component > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(5) > div:nth-child(2) > div')
+  total <- total + as.integer(gsub(",", "", day_5$getElementText()[[1]]))
+  
+  day_6 <- browser$findElement(using = 'css selector', '#main > div > div.main-content.ng-scope > div > div.ng-scope > react-component > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(6) > div:nth-child(2) > div')
+  total <- total + as.integer(gsub(",", "", day_6$getElementText()[[1]]))
+  
+  day_7 <- browser$findElement(using = 'css selector', '#main > div > div.main-content.ng-scope > div > div.ng-scope > react-component > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(7) > div:nth-child(2) > div')
+  total <- total + as.integer(gsub(",", "", day_7$getElementText()[[1]]))
+  
+  return(total)
+}
+
+stickiness <- function(browser, monthly_active) {
+  browser$navigate("https://amplitude.com/app/146509/home?range=Last%207%20Days&i=1&m=active&vis=line")
+  
+  daily_active <- browser$findElement(using = 'css selector', '.time-series-table-right > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(7)')
+  daily_active <-  as.integer(gsub(",", "", daily_active$getElementText()[[1]]))
+  
+  stickiness <- daily_active / monthly_active
+  
+  return(stickiness)
+}
+
+n_retention_day1 <- function(browser) {
+  browser$navigate("https://amplitude.com/app/146509/retention?se={%22event_type%22:%22_new%22,%22filters%22:[],%22group_by%22:[]}&re={%22event_type%22:%22_active%22,%22filters%22:[],%22group_by%22:[]}&cg=User&rm=nday&range=Last%203%20Days&i=1&sset={%22segmentIndex%22:0}&vis=line")
+  n_reten <- browser$findElement(using = 'css selector', '#main > div > div.main-content.ng-scope > div > div.ng-scope > react-component > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div')
+  n_reten <- as.numeric(gsub("%", "", n_reten$getElementText()[[1]]))
+}
+
+n_retention_day3 <- function(browser) {
+  browser$navigate("https://amplitude.com/app/146509/retention?se={%22event_type%22:%22_new%22,%22filters%22:[],%22group_by%22:[]}&re={%22event_type%22:%22_active%22,%22filters%22:[],%22group_by%22:[]}&cg=User&rm=nday&range=Last%205%20Days&i=1&sset={%22segmentIndex%22:0}&vis=line")
+  n_reten <- browser$findElement(using = 'css selector', 'react-component.ng-scope > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(2) > div:nth-child(1)')
+  n_reten <- as.numeric(gsub("%", "", n_reten$getElementText()[[1]]))
+}
+
+n_retention_day7 <- function(browser) {
+  browser$navigate("https://amplitude.com/app/146509/retention?se={%22event_type%22:%22_new%22,%22filters%22:[],%22group_by%22:[]}&re={%22event_type%22:%22_active%22,%22filters%22:[],%22group_by%22:[]}&cg=User&rm=nday&range=Last%209%20Days&i=1&sset={%22segmentIndex%22:0}&vis=line")
+  n_reten <- browser$findElement(using = 'css selector', 'react-component.ng-scope > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(8) > div:nth-child(2) > div:nth-child(1)')
+  n_reten <- as.numeric(gsub("%", "", n_reten$getElementText()[[1]]))
+}
+
+n_retention_day30 <- function(browser) {
+  browser$navigate("https://amplitude.com/app/146509/retention?se={%22event_type%22:%22_new%22,%22filters%22:[],%22group_by%22:[]}&re={%22event_type%22:%22_active%22,%22filters%22:[],%22group_by%22:[]}&cg=User&rm=nday&range=Last%2032%20Days&i=1&sset={%22segmentIndex%22:0}&vis=line")
+  n_reten <- browser$findElement(using = 'css selector', '#main > div > div.main-content.ng-scope > div > div.ng-scope > react-component > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(31) > div:nth-child(2) > div')
+  n_reten <- as.numeric(gsub("%", "", n_reten$getElementText()[[1]]))
+}
+
+unbounded_retention_day1 <- function(browser) {
+  browser$navigate("https://amplitude.com/app/146509/retention?se={%22event_type%22:%22_new%22,%22filters%22:[],%22group_by%22:[]}&re={%22event_type%22:%22_active%22,%22filters%22:[],%22group_by%22:[]}&cg=User&rm=rolling&range=Last%2030%20Days&i=1&sset={%22segmentIndex%22:0}&vis=line")
+  n_reten <- browser$findElement(using = 'css selector', 'react-component.ng-scope > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)')
+  n_reten <- as.numeric(gsub("%", "", n_reten$getElementText()[[1]]))
+}
+
+unbounded_retention_day3 <- function(browser) {
+  browser$navigate("https://amplitude.com/app/146509/retention?se={%22event_type%22:%22_new%22,%22filters%22:[],%22group_by%22:[]}&re={%22event_type%22:%22_active%22,%22filters%22:[],%22group_by%22:[]}&cg=User&rm=nday&range=Last%2032%20Days&i=1&sset={%22segmentIndex%22:0}&vis=line")
+  n_reten <- browser$findElement(using = 'css selector', 'react-component.ng-scope > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(2) > div:nth-child(1)')
+  n_reten <- as.numeric(gsub("%", "", n_reten$getElementText()[[1]]))
+}
+
+unbounded_retention_day7 <- function(browser) {
+  browser$navigate("https://amplitude.com/app/146509/retention?se={%22event_type%22:%22_new%22,%22filters%22:[],%22group_by%22:[]}&re={%22event_type%22:%22_active%22,%22filters%22:[],%22group_by%22:[]}&cg=User&rm=nday&range=Last%2032%20Days&i=1&sset={%22segmentIndex%22:0}&vis=line")
+  n_reten <- browser$findElement(using = 'css selector', 'react-component.ng-scope > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(8) > div:nth-child(2) > div:nth-child(1)')
+  n_reten <- as.numeric(gsub("%", "", n_reten$getElementText()[[1]]))
+}
+
+unbounded_retention_day30 <- function(browser) {
+  browser$navigate("https://amplitude.com/app/146509/retention?se={%22event_type%22:%22_new%22,%22filters%22:[],%22group_by%22:[]}&re={%22event_type%22:%22_active%22,%22filters%22:[],%22group_by%22:[]}&cg=User&rm=rolling&range=Last%2060%20Days&i=1&sset={%22segmentIndex%22:0}&vis=line")
+  n_reten <- browser$findElement(using = 'css selector', 'react-component.ng-scope > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(31) > div:nth-child(2) > div:nth-child(1)')
+  n_reten <- as.numeric(gsub("%", "", n_reten$getElementText()[[1]]))
+}
